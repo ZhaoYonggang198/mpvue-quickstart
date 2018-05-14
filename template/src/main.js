053bfe0db602db2bb467d0502773c6d0bae9f347
zhaoyonggang <yonggang.zhao@icloud.com>
2018-05-14 11:00:22 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -8,6 +8,8 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 App.mpType = 'app'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+import 'vuetify/dist/vuetify.css'
+
 const app = new Vue(App){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 app.$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
